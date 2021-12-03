d67ba08616c703006a59c0d2b8dba73c9b647b88
Sam Hocevar <sam@hocevar.net>
2021-12-03 09:54:53 +0100

    Use arrow notation for shorter code

--- src/transform.js
+++ src/transform.js
@@ -30,16 +30,12 @@ function plural_and_capital(word, src)
 
 function capital(word, src)
 {
-    if (/^\p{Lu}/u.test(src))
-        return word.charAt(0).toUpperCase() + word.slice(1);
-    return word;
+    return /^\p{Lu}/u.test(src) ? word.charAt(0).toUpperCase() + word.slice(1) : word;
 }
 
 function plural(word, src)
 {
-    if (/s$/.test(src))
-        return word + 's';
-    return word;
+    return /s$/.test(src) ? word + 's' : word;
 }
 
 var nft_replaces = ["shitty JPEG", "shoddy JPEG", "stinking JPEG"];
@@ -51,41 +47,33 @@ function transform(node)
     var v = node.nodeValue;
 
     // <start of sentence>NFT → Shitty JPEG
-    v = v.replace(/([.!?]\s+|^)NFTs?\b/g, function(m, p1) {
-        return p1 + plural_and_capital(randget(nft_replaces), 'A' + m);
-    });
+    v = v.replace(/([.!?]\s+|^)NFTs?\b/g,
+        (m, p1) => p1 + plural_and_capital(randget(nft_replaces), 'A' + m));
 
     // NFT → shitty JPEG
     // NFTs → shitty JPEGs
     // an NFT → a shitty JPEG
-    v = v.replace(/\b(([Aa])n(\s+))?NFTs?\b/g, function(m, p1, p2, p3) {
-        return (p1 ? p2 + p3 : '') + plural_and_capital(randget(nft_replaces), 'a' + m);
-    });
+    v = v.replace(/\b(([Aa])n(\s+))?NFTs?\b/g,
+        (m, p1, p2, p3) => (p1 ? p2 + p3 : '') + plural_and_capital(randget(nft_replaces), 'a' + m));
 
     // SomethingNFT → SomethingShittyJPEG
-    v = v.replace(/([A-Z][a-z]+)NFTs?\b/g, function(m, p1) {
-        return p1 + plural_and_capital(randget(nft_replaces), 'A' + m).replace(/\s+/g, '');
-    });
+    v = v.replace(/([A-Z][a-z]+)NFTs?\b/g,
+        (m, p1) => p1 + plural_and_capital(randget(nft_replaces), 'A' + m).replace(/\s+/g, ''));
 
-    v = v.replace(/[Nn]on[- ][Ff]ungible [Tt]okens?/g, function(m) {
-        return plural_and_capital(randget(non_fungible_token_replaces), m);
-    });
+    v = v.replace(/[Nn]on[- ][Ff]ungible [Tt]okens?/g,
+        (m) => plural_and_capital(randget(non_fungible_token_replaces), m));
 
-    v = v.replace(/\b[Bb]lockchains?\b/g, function(m) {
-        return plural_and_capital("circlejerk", m);
-    });
+    v = v.replace(/\b[Bb]lockchains?\b/g,
+        (m) => plural_and_capital("circlejerk", m));
 
-    v = v.replace(/\b[Cc]rypto[- ]?currenc(y|ies)\b/g, function(m) {
-        return capital("pretend money", m);
-    });
+    v = v.replace(/\b[Cc]rypto[- ]?currenc(y|ies)\b/g,
+        (m) => capital("pretend money", m));
 
-    v = v.replace(/\b[Pp]roof[-\s]+[Oo]f[-\s]+([Ww]ork)\b/g, function(m, p1) {
-        return capital("solving", m) + ' ' + capital("sudokus", p1);
-    });
+    v = v.replace(/\b[Pp]roof[-\s]+[Oo]f[-\s]+([Ww]ork)\b/g,
+        (m, p1) => capital("solving", m) + ' ' + capital("sudokus", p1));
 
-    v = v.replace(/\b([Bb])itcoins?\b/g, function(m) {
-        return plural_and_capital(randget(bitcoin_replaces), m);
-    });
+    v = v.replace(/\b([Bb])itcoins?\b/g,
+        (m) => plural_and_capital(randget(bitcoin_replaces), m));
 
     if (v != node.nodeValue)
         node.nodeValue = v;
